c80927e23c35cbaeb6558ef3586c22f390290935
Sebastien Ronsse <sronsse@gmail.com>
2013-03-03 13:59:59 -0800

    nes: cart is now handled by the nes mapper controller.

---
+++ main/resource.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <resource.h>
+
+struct resource *resource_get(char *name, struct resource *resources,
+	int num_resources)
+{
+	int i;
+	for (i = 0; i < num_resources; i++)
+		if (!strcmp(name, resources[i].name))
+			return &resources[i];
+	return NULL;
+}
+
